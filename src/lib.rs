c0d34ccbf4681e3e851503a1b4cc9d23364b4751
clflushopt <172141496+clflushopt@users.noreply.github.com>
2025-05-24 16:31:31 -0400

    feat: implement a core tpch table func to generate all data

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 use datafusion::arrow::compute::concat_batches;
+use datafusion::arrow::datatypes::Schema;
 use datafusion::catalog::{TableFunctionImpl, TableProvider};
 use datafusion::common::{Result, ScalarValue, plan_err};
 use datafusion::datasource::memory::MemTable;
 use datafusion::prelude::SessionContext;
+use datafusion::sql::TableReference;
 use datafusion_expr::Expr;
+use std::fmt::Debug;
 use std::sync::Arc;
 use tpchgen_arrow::RecordBatchIterator;
 
@@ -194,6 +197,127 @@ pub fn register_tpch_udtfs(ctx: &SessionContext) -> Result<()> {
     Ok(())
 }
 
+struct TpchTableProvider {
+    ctx: SessionContext,
+}
+
+impl TpchTableProvider {
+    /// Creates a new TPCH table provider.
+    pub fn new(ctx: SessionContext) -> Self {
+        Self { ctx }
+    }
+}
+
+// Implement the `TableProvider` trait for the `TpchTableProvider`, we need
+// to do it manually because the `SessionContext` does not implement it.
+impl Debug for TpchTableProvider {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "TpchTableProvider")
+    }
+}
+
+impl TableFunctionImpl for TpchTableProvider {
+    /// The `call` method is the entry point for the UDTF and is called when the UDTF is
+    /// invoked in a SQL query.
+    ///
+    /// It takes a list of arguments, the scale factor, whether to generate the data on
+    /// disk in parquet format and the path to the output files. If no path is provided,
+    /// the data is generated in memory and we fallback to the `MemTable` provider.
+    fn call(&self, args: &[Expr]) -> Result<Arc<dyn TableProvider>> {
+        let scale_factor = match args.first() {
+            Some(Expr::Literal(ScalarValue::Float64(Some(value)))) => *value,
+            _ => return plan_err!("First argument must be a float literal."),
+        };
+
+        let write_to_disk = match args.get(1) {
+            Some(Expr::Literal(ScalarValue::Boolean(Some(value)))) => *value,
+            _ => false,
+        };
+
+        let path = match args.get(2) {
+            Some(Expr::Literal(ScalarValue::Utf8(Some(value)))) => value.clone(),
+            _ => "".to_string(),
+        };
+
+        // Short path when `write_to_disk` is false or `path` is empty.
+        if !write_to_disk || path.is_empty() {
+            //// Start with nation table.
+            let nation_provider = TpchNation {};
+            let nation_table = nation_provider
+                .call(vec![Expr::Literal(ScalarValue::Float64(Some(scale_factor)))].as_slice())?;
+            //// Register the table in the session context.
+            self.ctx
+                .register_table(TableReference::bare("tpch_nation"), nation_table)?;
+
+            // Register the rest of the tables in the session context.
+            let customer_provider = TpchCustomer {};
+            let customer_table = customer_provider
+                .call(vec![Expr::Literal(ScalarValue::Float64(Some(scale_factor)))].as_slice())?;
+            self.ctx
+                .register_table(TableReference::bare("tpch_customer"), customer_table)?;
+            let orders_provider = TpchOrders {};
+            let orders_table = orders_provider
+                .call(vec![Expr::Literal(ScalarValue::Float64(Some(scale_factor)))].as_slice())?;
+            self.ctx
+                .register_table(TableReference::bare("tpch_orders"), orders_table)?;
+            let lineitem_provider = TpchLineitem {};
+            let lineitem_table = lineitem_provider
+                .call(vec![Expr::Literal(ScalarValue::Float64(Some(scale_factor)))].as_slice())?;
+            self.ctx
+                .register_table(TableReference::bare("tpch_lineitem"), lineitem_table)?;
+            let part_provider = TpchPart {};
+            let part_table = part_provider
+                .call(vec![Expr::Literal(ScalarValue::Float64(Some(scale_factor)))].as_slice())?;
+            self.ctx
+                .register_table(TableReference::bare("tpch_part"), part_table)?;
+            let partsupp_provider = TpchPartsupp {};
+            let partsupp_table = partsupp_provider
+                .call(vec![Expr::Literal(ScalarValue::Float64(Some(scale_factor)))].as_slice())?;
+            self.ctx
+                .register_table(TableReference::bare("tpch_partsupp"), partsupp_table)?;
+            let supplier_provider = TpchSupplier {};
+            let supplier_table = supplier_provider
+                .call(vec![Expr::Literal(ScalarValue::Float64(Some(scale_factor)))].as_slice())?;
+            self.ctx
+                .register_table(TableReference::bare("tpch_supplier"), supplier_table)?;
+            let region_provider = TpchRegion {};
+            let region_table = region_provider
+                .call(vec![Expr::Literal(ScalarValue::Float64(Some(scale_factor)))].as_slice())?;
+            self.ctx
+                .register_table(TableReference::bare("tpch_region"), region_table)?;
+
+            // Create a table with the schema |table_name| and the data is just the
+            // individual table names.
+            let schema = Schema::new(vec![datafusion::arrow::datatypes::Field::new(
+                "table_name",
+                datafusion::arrow::datatypes::DataType::Utf8,
+                false,
+            )]);
+            let batch = datafusion::arrow::record_batch::RecordBatch::try_new(
+                Arc::new(schema.clone()),
+                vec![Arc::new(datafusion::arrow::array::StringArray::from(vec![
+                    "tpch_nation",
+                    "tpch_customer",
+                    "tpch_orders",
+                    "tpch_lineitem",
+                    "tpch_part",
+                    "tpch_partsupp",
+                    "tpch_supplier",
+                    "tpch_region",
+                ]))],
+            )?;
+            let mem_table = MemTable::try_new(Arc::new(schema), vec![vec![batch]])?;
+
+            return Ok(Arc::new(mem_table));
+        }
+
+        // Call the UDTF with the given arguments.
+        let table = TpchNation {};
+        let provider = table.call(args)?;
+        Ok(provider)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -202,6 +326,9 @@ mod tests {
     #[tokio::test]
     async fn test_register_all_tpch_functions() -> Result<()> {
         let ctx = SessionContext::new();
+
+        let tpch_tbl_fn = TpchTableProvider::new(ctx.clone());
+        ctx.register_udtf("tcph", Arc::new(tpch_tbl_fn));
 
         // Register all the UDTFs.
         register_tpch_udtfs(&ctx)?;
@@ -275,6 +402,62 @@ mod tests {
         for (function, expected_rows, expected_columns) in test_cases {
             let df = ctx
                 .sql(&format!("SELECT * FROM {}(1.0)", function))
+                .await?
+                .collect()
+                .await?;
+
+            assert_eq!(df.len(), 1);
+            assert_eq!(
+                df[0].num_rows(),
+                expected_rows,
+                "{}: {}",
+                function,
+                expected_rows
+            );
+            assert_eq!(
+                df[0].num_columns(),
+                expected_columns,
+                "{}: {}",
+                function,
+                expected_columns
+            );
+        }
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_register_tpch_provider() -> Result<()> {
+        let ctx = SessionContext::new();
+
+        // Register the TPCH provider.
+        let tpch_provider = TpchTableProvider::new(ctx.clone());
+        ctx.register_udtf("tpch", Arc::new(tpch_provider));
+
+        // Test the TPCH provider.
+        let df = ctx
+            .sql("SELECT * FROM tpch(1.0, false, '')")
+            .await?
+            .collect()
+            .await?;
+
+        assert_eq!(df.len(), 1);
+        assert_eq!(df[0].num_rows(), 8);
+        assert_eq!(df[0].num_columns(), 1);
+
+        let test_cases = vec![
+            (TpchNation::name(), 25, 4),
+            (TpchCustomer::name(), 150000, 8),
+            (TpchOrders::name(), 1500000, 9),
+            (TpchLineitem::name(), 6001215, 16),
+            (TpchPart::name(), 200000, 9),
+            (TpchPartsupp::name(), 800000, 5),
+            (TpchSupplier::name(), 10000, 7),
+            (TpchRegion::name(), 5, 3),
+        ];
+
+        for (function, expected_rows, expected_columns) in test_cases {
+            let df = ctx
+                .sql(&format!("SELECT * FROM {}", function))
                 .await?
                 .collect()
                 .await?;
